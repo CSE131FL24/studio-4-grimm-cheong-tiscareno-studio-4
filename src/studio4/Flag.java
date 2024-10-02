998ae19740bb59e2739e96dc49c923b3cc889b74
maddiegrimm <maddiegrimm@localhost>
2024-10-02 14:10:23 -0500

    finished studio!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,22 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color lightBlue = new Color(137, 207, 240);
+		Color coral = new Color(255, 178, 127);
+		Color mintGreen = new Color(189, 252, 201);
 		
+		StdDraw.setPenColor(mintGreen);
+		StdDraw.filledRectangle(0.5, 0.5, 0.35, 0.25);
+		
+		StdDraw.setPenColor(coral);
+		StdDraw.setPenRadius(0.025);
+		StdDraw.line(0.25, 0.75, 0.25, 0.25);
+		StdDraw.line(0.75, 0.75, 0.75, 0.25);
+		
+		StdDraw.line(0.15, 0.25, 0.85, 0.25);
+		StdDraw.line(0.15, 0.75, 0.85, 0.75);
+		
+		StdDraw.setPenColor(lightBlue);
+		StdDraw.filledCircle(0.5, 0.5, 0.15);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -9,6 +9,8 @@ import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
 
+import java.awt.Color;
+
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
@@ -20,5 +22,58 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		
+		Color color = new Color(redComponent, greenComponent, blueComponent);
+		StdDraw.setPenColor(color);
+		
+		if (shapeType.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if (isFilled) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+		else if (shapeType.equals("ellipse")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			
+			if (isFilled) {
+				StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
+		else {
+			double x1 = in.nextDouble();
+			double y1 = in.nextDouble();
+			double x2 = in.nextDouble();
+			double y2 = in.nextDouble();
+			double x3 = in.nextDouble();
+			double y3 = in.nextDouble();
+			
+			double[] x = {x1, x2, x3};
+			double[] y = {y1, y2, y3};
+			
+			if (isFilled) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
